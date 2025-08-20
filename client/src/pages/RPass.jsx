6fa6b568c420ec333fb5067d95dc4c6a24c77993
zdawan <kumarjothi704@gmail.com>
2025-08-20 21:16:08 +0530

    First  commit

--- client/src/pages/RPass.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function RPass() {
-  return <h1>Reset Password Page</h1>;
-}
--- client/src/pages/VEmail.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function VEmail() {
-  return <h1>Verify Email Page</h1>;
-}
